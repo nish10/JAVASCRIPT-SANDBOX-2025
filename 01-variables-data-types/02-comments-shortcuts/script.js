2593c28fdc0390f9cbcd9db1a18c547ef83c2c03
nishkuma <9835269610_@#NA>
2025-02-13 15:36:35 +0530

    02-comments-shortcuts done

---
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,35 @@
+// This is a single line of code
+
+/*
+Multi line comment
+
+console.log(10);
+
+console.log('Hello World');
+*/
+
+console.log(100);
+
+console.log('Hello World');
+
+console.log(29, 'Hi', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({name: 'Nishant', email: 'nishant.raj334@gmail.com'});
+
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
