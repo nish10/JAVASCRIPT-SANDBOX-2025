097a647bfe215ea92f267a9ec8a04d7d5e09fee8
nishkuma <9835269610_@#NA>
2025-02-13 15:10:23 +0530

    01-console done

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,29 @@
+console.log(100);
+
+console.log('Hello World');
+
+console.log(29, 'Hi', true);
+
+const x = 100;
+
+console.log(x);
+
+console.error('Alert');
+
+console.warn('Warning');
+
+console.table({name: 'Nishant', email: 'nishant.raj334@gmail.com'});
+
+console.group('simple');
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
+
+
+
+
