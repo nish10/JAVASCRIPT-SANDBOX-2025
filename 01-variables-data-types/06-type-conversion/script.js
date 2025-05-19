4f45aac315a5afd55f13aa23dab977bd726210f4
nish01 <nishant.raj334@gmail.com>
2025-05-20 00:51:44 +0530

    type conversion

---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,33 @@
+let amount = '100';
+
+amount = '99.5';
+
+amount = 1;
+
+amount = 0;
+
+amount = 'hello';
+
+// change string to number
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+
+// change a number to string
+// amount = amount.toString();
+// amount = String(amount);
+
+// change string to decimal
+// amount = parseInt(amount);
+// amount = parseFloat(amount);
+
+// convert number to boolean
+// amount = Boolean(amount);
+
+// console.log(Math.sqrt(-1));
+
+console.log(1 + NaN);
+console.log(undefined + undefined);
+console.log('foo' / 3);
+
+console.log(amount, typeof amount);
