904b7b4cf08c28039a29fd6ceab9e1a5c641ff01
nish01 <nishant.raj334@gmail.com>
2025-05-20 02:29:13 +0530

    date abd time

---
+++ 01-variables-data-types/14-dates-and-times/script.js
@@ -0,0 +1,21 @@
+let x;
+
+x = new Date();
+
+x = x.toString();
+
+x = new Date(2025, 4, 20, 12, 30, 0);
+
+x = new Date('2025-05-20T12:30:00');
+
+x = Date.now();
+
+// x = new Damte('07-10-2022');
+
+// x = x.getTime();
+
+x = x.valueOf();
+
+x = new Date(x);
+
+console.log(x);
