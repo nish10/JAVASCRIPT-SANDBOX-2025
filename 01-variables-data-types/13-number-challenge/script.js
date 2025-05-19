8f43f4a6fb92791d4ed78080936a6a4114d6fcc1
nish01 <nishant.raj334@gmail.com>
2025-05-20 02:19:21 +0530

    number challenge

---
+++ 01-variables-data-types/13-number-challenge/script.js
@@ -0,0 +1,13 @@
+let x = Math.floor(Math.random() * 100 + 1);
+
+console.log(x);
+
+let y = Math.floor(Math.random() * 50 + 1);
+
+console.log(y);
+
+console.log(`${x} + ${y} = ${x + y}`);
+console.log(`${x} - ${y} = ${x - y}`);
+console.log(`${x} * ${y} = ${x * y}`);
+console.log(`${x} / ${y} = ${x / y}`);
+console.log(`${x} % ${y} = ${x + y}`);
