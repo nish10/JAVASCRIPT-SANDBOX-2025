c1f9f2f4d98d05ae8d2b4ec22a8550b3f20db8c6
nish01 <nishant.raj334@gmail.com>
2025-05-21 17:13:05 +0530

    arrays basics

---
+++ 02-arrays-and-objects/01-arrays-basics/script.js
@@ -0,0 +1,26 @@
+// Array Literal
+const numbers = [12, 45, 33, 29, 30];
+const mixed = ['nish', true, null, 1];
+
+// Array Constructor
+const fruits = new Array('apple', 'grape', 'banana');
+
+console.log(numbers);
+console.log(fruits);
+
+x = numbers[0];
+
+x = numbers[0] + numbers[3];
+
+x = `My faviourite number is ${numbers[0]}`;
+
+x = numbers.length;
+
+fruits[2] = 'pear';
+
+fruits[fruits.length] = 'blueberry';
+fruits[fruits.length] = 'peach';
+
+console.log(x);
+console.log(fruits);
+console.log(mixed);
