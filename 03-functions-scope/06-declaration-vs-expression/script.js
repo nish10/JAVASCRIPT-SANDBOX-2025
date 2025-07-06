c86b6580bb5529f492940d37ee1ab1d609c0c54d
Nishant <nishant.raj334@gmail.com>
2025-07-06 14:11:28 +0530

    declaration-vs-expression

---
+++ 03-functions-scope/06-declaration-vs-expression/script.js
@@ -0,0 +1,15 @@
+console.log(addDollarSign(100));
+
+// Function Declaration
+function addDollarSign(value) {
+  return '$' + value;
+}
+
+// console.log(addDollarSign(100));
+
+console.log(addPlusSign(200));
+
+// Function Expression
+const addPlusSign = function (value) {
+  return '+' + value;
+};
