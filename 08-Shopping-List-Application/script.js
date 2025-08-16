f58904ed76d79eb055296c49370e74daf95944ab
nish01 <nishant.raj334@gmail.com>
2025-08-16 15:29:07 +0530

    remove clear items

--- 08-Shopping-List-Application/script.js
+++ 08-Shopping-List-Application/script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
 
 function createIcon(classes) {
   const icon = document.createElement('i');
@@ -47,6 +48,22 @@ function addItem(e) {
 
   itemInput.value = '';
 }
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    console.log('click');
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearItems(e) {
+  //   itemList.innerHTML = '';
+  //  OR
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+}
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
