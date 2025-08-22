5278a81e0a3906cd9db20e20860d7fae41272465
nish01 <nishant.raj334@gmail.com>
2025-08-22 15:39:14 +0530

    fetch API basics

---
+++ 09-asynchronous-javascript/12-fetch-and-async-await/01-fetch-basics/script.js
@@ -0,0 +1,27 @@
+// fetching a json file
+fetch('./movies.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+// Fetching a text file
+fetch('./test.txt')
+  .then((response) => {
+    return response.text();
+  })
+  .then((text) => {
+    console.log(text);
+  });
+
+// Fetching from an API, if dont specify it will be by defualt GET Request
+fetch('https://api.github.com/users/nish10')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    document.querySelector('h1').textContent = data.login;
+  });
