54fffb7383854f390247278dce1c1252ab600e1b
nish01 <nishant.raj334@gmail.com>
2025-08-24 13:56:06 +0530

    typicode todo app part 1

---
+++ 10-fetch-async-await/04-typicode-todos-mini-project/script.js
@@ -0,0 +1,53 @@
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+const getTodos = () => {
+  fetch(apiUrl + '?_limit=5')
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((todo) => {
+        addTodoToDOM(todo);
+      });
+    });
+};
+
+const addTodoToDOM = (todo) => {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id);
+
+  console.log(div);
+
+  if (todo.completed) {
+    div.classList.add('done');
+  }
+
+  document.getElementById('todo-list').appendChild(div);
+};
+
+const createTodo = (e) => {
+  e.preventDefault();
+
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false,
+  };
+
+  console.log(newTodo);
+
+  fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => addTodoToDOM(data));
+};
+
+const init = () => {
+  document.addEventListener('DOMContentLoaded', getTodos);
+  document.querySelector('#todo-form').addEventListener('submit', createTodo);
+};
+
+init();
