67f848b7a5f9449a6c87dd9e7dc228142d8f62b2
Nishant <nishant.raj334@gmail.com>
2025-08-29 14:52:19 +0530

    CommonJS module

--- node-playground/app.js
+++ node-playground/app.js
@@ -1,9 +1,21 @@
 // console.log('Hello, Node.js!'); -- commaand to run this node app: node app.js
 
-async function getUser() {
-  const response = await fetch('https://api.github.com/users/nish10');
-  const data = await response.json();
-  console.log(data);
-}
+// async function getUser() {
+//   const response = await fetch('https://api.github.com/users/nish10');
+//   const data = await response.json();
+//   console.log(data);
+// }
 
-getUser();
+// getUser();
+
+const { capitalizeWords, makeMoney } = require('./utils');
+
+console.log(capitalizeWords('hello world from node.js'));
+
+console.log(makeMoney(1000));
+
+const Person = require('./Person');
+
+const person1 = new Person('Nishant', 28);
+
+person1.greet();
